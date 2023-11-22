9549ec0fb7ec5029e564ce40510ee7ab1cf6384c
Elisa Rose Madsen <101463323@georgebrown.ca>
2023-11-22 13:20:12 +0100

    Test

---
+++ src/main/java/com/example/hovedopgavekea/HovedopgaveKeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hovedopgavekea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HovedopgaveKeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HovedopgaveKeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hovedopgavekea/HovedopgaveKeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hovedopgavekea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HovedopgaveKeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
